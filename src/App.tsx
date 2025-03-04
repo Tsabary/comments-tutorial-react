4cee2ecb81128efb99b90190ca837b473aa5c6d0
Yanay Zabary <yanay.tsabary@gmail.com>
2025-03-04 13:38:33 +0200

    added replyke and dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { ReplykeProvider } from "@replyke/react-js";
+import { Toaster } from "@/components/ui/sonner";
+import { posts } from "./constants/dummy-data";
+
 function App() {
-  return <div className="min-h-screen bg-red-500">Hello World</div>;
+  return (
+    <ReplykeProvider projectId={import.meta.env.VITE_PUBLIC_REPLYKE_PROJECT_ID}>
+      <Toaster />
+      <div className="h-screen grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-12 gap-4 md:gap-8 lg:gap-12 m-auto">
+        {posts.map((post) => (
+          <div className="bg-white p-4 mb-4 rounded-xl shadow-lg">
+            <p className="text-lg font-bold text-gray-800">{post.id}</p>
+            <p className="text-gray-600 mt-2">{post.content}</p>
+          </div>
+        ))}
+      </div>
+    </ReplykeProvider>
+  );
 }
 
 export default App;
---
+++ src/constants/dummy-data.ts
@@ -0,0 +1,23 @@
+// Dummy data for posts
+const posts = [
+  { id: "post1", content: "This is the first post." },
+  { id: "post2", content: "Hello world! This is post two." },
+  { id: "post3", content: "Another day, another post." },
+  { id: "post4", content: "Simple content for post four." },
+  { id: "post5", content: "Post five is here." },
+  { id: "post6", content: "Sharing thoughts in post six." },
+  { id: "post7", content: "This is the seventh post." },
+  { id: "post8", content: "Post eight with simple text." },
+  { id: "post9", content: "The ninth post is ready." },
+  { id: "post10", content: "Finally, this is the tenth post." },
+];
+
+// Dummy data for users
+const users = [
+  { id: "user1", username: "lionel_messi10" },
+  { id: "user2", username: "diego_maradona" },
+  { id: "user3", username: "pele" },
+  { id: "user4", username: "ronaldo" },
+];
+
+export { posts, users };
